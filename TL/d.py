79bb83e44ff25cb1601a2abaacb45ba941d3b8e1
albertlin11 <52873299+albertlin11@users.noreply.github.com>
2021-12-09 14:36:10 +0800

    Delete TL directory

--- TL/d.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Nov  6 11:00:50 2021
-
-@author: HP ED-800
-"""
-import numpy as np
-a= np.array([0.09090909, 0.32467532, 0.        , 1.        , 0.        ,       0.        ])
